7622c2824cbb632901ee13ddc7506c79fc4e928b
Voltsuuu <npoudel2007@gmail.com>
2023-10-15 22:11:35 -0400

    done

---
+++ src/HighOrLow.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class HighOrLow {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int rand = (int)((Math.random() * 10) + 1);
+        int guess;
+
+        System.out.println("Welcome! Let's play a game. Try to guess the number I'm thinking of!");
+        do {
+            if (scan.hasNextDouble()) {
+                guess = scan.nextInt();
+                if (guess >= 0) {
+                    break;
+                }
+            }
+            scan.nextLine();
+            System.out.println("Please enter a valid input");
+        } while (true);
+
+        if (guess > rand) {
+            System.out.println("Your guess was too high");
+            System.out.println("The correct answer was: " + rand);
+        } else if (guess < rand) {
+            System.out.println("Your guess was too low");
+            System.out.println("The correct answer was: " + rand);
+        } else {
+            System.out.println("Your guess was right!");
+            System.out.println("The correct answer was: " + rand);
+        }
+    }
+}
